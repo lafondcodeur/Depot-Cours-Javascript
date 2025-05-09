692466bb2ba950464917bd25b9203469a0c1a49f
lafond <macklafonddeboumas>
2025-05-09 11:16:26 +0100

    Commit du premier cours de Javascript

---
+++ Index.js
@@ -0,0 +1,41 @@
+//    let nom_de_la_variable = valeur(nombre entier, réel, chaîne de caractères, booléen);
+
+// Ceci es une déclaration d'une variable en JavaScript de type entier
+let nombre_entier = 3;
+nombre_entier = 500000000;
+
+// Ceci es une déclaration d'une variable en JavaScript de type réel
+// (nombre à virgule flottante)
+let nombre_reel = 4.5;
+nombre_reel = 3.14;
+
+// Ceci es une déclaration d'une variable en JavaScript de type chaîne de caractères
+let chaine_caractere = "Hello World!";
+chaine_caractere = "Bonjour le monde!";
+
+// ceci es une déclaration d'une variable en JavaScript  de type booléen
+// (true ou false)
+let variable_booleen = true;
+variable_booleen = false;
+
+// Les constantes sont des variables dont la valeur ne peut pas être modifiée
+
+// const nom_de_la_constante = valeur(nombre entier, réel, chaîne de caractères, booléen);
+const Pi = 3.178;
+// Pi = 2.14;
+const nom = "MACK LAFOND";
+
+//Les op&érateurs arithmétiques
+// +, -, *, /, % (modulo)
+
+// + addition
+let a = 5;
+let b = 3;
+let somme = a + b;
+console.log(somme);
+
+// - soustraction
+a = 200;
+b = 50;
+let soustraction = a - b;
+console.log(soustraction);
